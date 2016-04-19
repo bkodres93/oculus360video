c2664812e6d123a8f2258e620c0ad2063435f4d5
bkodres93 <b.kodres@gmail.com>
2016-04-18 22:17:48 -0400

    Heres the stuff

---
+++ AudioScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+	Transform myTransform;
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		// face the audio towards the center
+		// ideally it would face the "camera" which happens to be at the center
+		myTransform = ((Transform)GetComponent<Renderer>().transform);
+		myTransform.LookAt(Vector3.zero);
+
+		source = ((AudioSource)GetComponent<AudioSource>());
+
+		// a miracle happens
+
+		//source.volume = 1 - Quaternion.Angle(Transform(Camera.main).rotation, angleToAudioSrc) / 180f;
+		source.volume = 1;
+		source.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PlayMovieScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovieScript : MonoBehaviour {
+	MovieTexture movie;
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		movie = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);
+		movie.Play();
+		movie.loop = true;
+		source = ((AudioSource)GetComponent<AudioSource>());
+		source.Play();
+		source.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown ("Jump")) {
+            
+            if (movie.isPlaying) {
+                movie.Pause();
+				source.Pause();
+            }
+            else {
+                movie.Play();
+				source.Play();
+            }
+        }
+    }
+}
